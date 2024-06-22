67b900c3c714d51f28dffba4174f09d45eb77485
coucoudu71 <technodu71@gmail.com>
2024-06-22 17:45:27 +0200

    Nouveau fonctionnement grace à l'accelerometre

--- TestComplet/TestComplet.ino
+++ TestComplet/TestComplet.ino
@@ -3,35 +3,23 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
-#include <MadgwickAHRS.h>
 #include <Math.h>
 
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 128 // OLED display height, in pixels
 #define OLED_RESET -1     // can set an oled reset pin if desired
 Adafruit_SH1107 display = Adafruit_SH1107(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET, 1000000, 100000);
-float pitchFilteredOld;
-float rollFilteredOld;
-Madgwick filter;
-const float sensorRate = 104.00;
 
-void testdrawchar(char* x, char* y, char* z) {
+void welcomeGoogly() {
   display.clearDisplay();
-
-  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextSize(3);      // Normal 1:1 pixel scale
   display.setTextColor(SH110X_WHITE); // Draw white text
-  display.setCursor(0, 20);     // Start at top-left corner
+  display.setCursor(30, 0);     // Start at top-left corner
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
 
-  // Not all the characters will fit on the display. This is normal.
-  // Library will draw what it can and the rest will be clipped.
-  display.write(x);
-  display.write('\n');
-  display.write(y);
-  display.write('\n');
-  display.write(z);
-  display.write('\n');
+  display.write("GOOGLY");
   display.display();
+  delay(2000);
 }
 
 #define rayonCercle 20
@@ -51,25 +39,21 @@ void setup()   {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
-  filter.begin(sensorRate);
   Serial.println("Setup complete!");
   
 
   Serial.print("Gyroscope sample rate = ");
-  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.print(IMU.accelerationSampleRate());
   Serial.println(" Hz");
   Serial.println();
   Serial.println("Gyroscope in degrees/second");
   Serial.println("X\tY\tZ");
-  filter.begin(sensorRate);
   Serial.println("Setup complete!");
 
-  delay(250); // wait for the OLED to power up
+  welcomeGoogly();
+  //delay(250); // wait for the OLED to power up
   display.begin(0x3C, true); // Address 0x3D default
-  display.display();
-  delay(2000);
   display.clearDisplay();
-  testdrawchar("TEST","OK","1");      // Draw characters of the default font
 }
 
 float pitchOld, rollOld;
@@ -77,21 +61,18 @@ float pitchOld, rollOld;
 void loop()
 {
   float xAcc, yAcc, zAcc;
-  float xGyro, yGyro, zGyro;
   float roll, pitch;
   int X = 64;
   int Y = 64;
-  if(IMU.accelerationAvailable() && IMU.gyroscopeAvailable()){
+  if(IMU.accelerationAvailable()){
     IMU.readAcceleration(xAcc, yAcc, zAcc);
-    IMU.readGyroscope(xGyro, yGyro, zGyro); 
-    filter.updateIMU(xGyro, yGyro, zGyro, xAcc, yAcc, zAcc);
-    pitch = filter.getPitch();
-    roll = filter.getRoll();
+    pitch = -atan(xAcc/zAcc)*180/3.1415;
+    roll = atan(yAcc/zAcc)*180/3.1415;
 
     //float pitchFiltered = 0.1 * pitch + 0.9 * pitchFilteredOld; // low pass filter
     //pitchFilteredOld = pitchFiltered;
     ///////////////////////////////////////PITCH X////////////////////////////////////////////////
-    if(pitch>pitchOld  && (X+pitch)<128 || pitch<pitchOld && (X+pitch)>0)
+    if(pitch>pitchOld  && (X+pitch)<128-rayonCercle || pitch<pitchOld && (X+pitch)>rayonCercle)
     {
       X+=pitch;
       pitchOld=pitch;
@@ -103,7 +84,7 @@ void loop()
     }
 
     ////////////////////////////////ROLL Y//////////////////////////////////////////////
-    if(roll>rollOld && (Y+roll)<128 || roll<rollOld && (Y+roll)>0)
+    if(roll>rollOld && (Y+roll)<128-rayonCercle || roll<rollOld && (Y+roll)>rayonCercle)
     {
       Y+=roll;
       rollOld=roll;
@@ -114,6 +95,7 @@ void loop()
     }
 
     affiche(X,Y);
+    delay(10);
     Serial.println("PITCH : " + String(pitch) + "       ROLL : " + String(roll) + "  " + String(X) + "  " + String(Y) + "    " + String(rollOld));
     
 
